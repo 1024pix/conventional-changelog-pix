aa06d5688430a6aee7b11ac52bb6098ba1d4f169
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-10-02 23:37:52 +0200

    chore: add conventional-changelog-ember

---
+++ writerOpts.js
@@ -0,0 +1,53 @@
+import { readFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export async function createWriterOpts () {
+  const [template, header, commit] = await Promise.all([
+    readFile(resolve(dirname, './templates/template.hbs'), 'utf-8'),
+    readFile(resolve(dirname, './templates/header.hbs'), 'utf-8'),
+    readFile(resolve(dirname, './templates/commit.hbs'), 'utf-8')
+  ])
+  const writerOpts = getWriterOpts()
+
+  writerOpts.mainTemplate = template
+  writerOpts.headerPartial = header
+  writerOpts.commitPartial = commit
+
+  return writerOpts
+}
+
+function getWriterOpts () {
+  return {
+    transform: (commit) => {
+      if (!commit.pr) {
+        return
+      }
+
+      if (commit.tag === 'BUGFIX') {
+        commit.tag = 'Bug Fixes'
+      } else if (commit.tag === 'CLEANUP') {
+        commit.tag = 'Cleanup'
+      } else if (commit.tag === 'FEATURE') {
+        commit.tag = 'Features'
+      } else if (commit.tag === 'DOC') {
+        commit.tag = 'Documentation'
+      } else if (commit.tag === 'SECURITY') {
+        commit.tag = 'Security'
+      } else {
+        return
+      }
+
+      if (typeof commit.hash === 'string') {
+        commit.shortHash = commit.hash.substring(0, 7)
+      }
+
+      return commit
+    },
+    groupBy: 'tag',
+    commitGroupsSort: 'title',
+    commitsSort: ['tag', 'taggedAs', 'message']
+  }
+}
