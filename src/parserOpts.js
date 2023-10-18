6d5fff99d7b7e44952ca7c7bb98b7a134e8e29ff
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-10-18 15:36:12 +0200

    feat: use only merge commits

--- src/parserOpts.js
+++ src/parserOpts.js
@@ -1,7 +1,7 @@
 export function createParserOpts () {
   return {
     gitRawCommitsOpts: {
-      noMerges: null
+      merges: true
     },
     headerPattern: /^\[(.*)] (.*)$/,
     headerCorrespondence: [
